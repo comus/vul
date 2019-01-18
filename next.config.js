a3bdfb5bcc0be32d6090751815a11a730ecf2464
comus <comus.leong@gmail.com>
2019-01-19 01:33:03 +0800

    index.browser browser entry point

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   target: 'serverless',
   webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
     if (!isServer) {
-      config.resolve.mainFiles = ['index-client', 'index']
+      config.resolve.mainFiles = ['index.browser', 'index']
     }
     config.resolve.modules = [path.resolve(__dirname, 'src'), 'node_modules']
     config.resolve.alias['src'] = path.join(__dirname, 'src')
